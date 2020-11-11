ded404eb7414c5731a21ee74996fe0e32b804b3e
Matthew Quint <mattquint111@gmail.com>
2020-11-11 13:37:57 -0600

    console mock-up

--- super-react-boy/src/App.js
+++ super-react-boy/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainCont'>
+      <div className="gameConsole">
+
+        <div className="headerCont">
+          <div className="colorBarsLeft"></div>
+          <p className='headerText'>DOT MATRIX WITH STEREO SOUND</p>
+          <div className="colorBarsRight"></div>
+        </div>
+
+        <div className="outerScreen">
+
+          <div className='batteryCont'>
+            <div className='power'></div>
+            <p className="powerText">BATTERY</p>
+          </div>
+
+          <div className="innerScreen">
+
+          </div>
+
+        </div>
+
+        <div className="controlsCont">
+
+          <div className='directionCont'>
+            <h1 className="directionPad">+</h1>
+          </div>
+
+          <div className="buttonCont">
+            <button className="gameButton-X"></button>
+            <button className="gameButton-Y"></button>
+          </div>
+
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
