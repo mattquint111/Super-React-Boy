a7583d022f6210f335befd2bc0a10a4b5b916fd0
Matthew Quint <mattquint111@gmail.com>
2020-11-12 12:13:04 -0600

    console reformatting

--- super-react-boy/src/App.js
+++ super-react-boy/src/App.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect, useHistory } from 'react'
 
-
-
-
 function App() {
 
-  const history = useHistory();
-
-  const startGame = (e) => {
-    history.push('/add-movie')
-  }
-
   return (
-    <div className='homepageMainCont' onClick={startGame}>
-      <h1 className="homepageTitle pretendo">Super-React-Boy</h1>
-      <h2 className="pretendo">Press any key to start</h2>
+    <div className='homepageMainCont'>
+      <h1 className="homepageTitle pretendo">Super-React-Boy <sub className="trademark">TM</sub></h1>
+      <button className="startBtn btn">Click to start</button>
     </div>
   )
+
 }
 
 export default App
--- super-react-boy/src/index.js
+++ super-react-boy/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import AddMovie from './components/AddMovie'
 import BaseLayout from './components/BaseLayout'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,6 @@ ReactDOM.render(
       <BaseLayout>
         <Switch>
           <Route component = {App} path="/" exact/>
-          <Route component = {AddMovie} path="/add-movie"/>
         </Switch>
       </BaseLayout>
     </BrowserRouter>
