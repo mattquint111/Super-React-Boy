7bb4712fdae56683d3f17e3307c50f2a84c31d56
Matthew Quint <mattquint111@gmail.com>
2020-11-21 17:19:33 -0600

    homepage formatting

--- super-react-boy/src/components/BaseLayout.js
+++ super-react-boy/src/components/BaseLayout.js
@@ -7,25 +7,69 @@ function BaseLayout(props) {
         <div>
             <div className="mainCont">
                 <div className="gameConsole">
-                    <div className="headerCont">
-                        <div className="colorBarsLeft"></div>
-                        <p className="headerText">
-                            DOT MATRIX WITH STEREO SOUND
-                        </p>
-                        <div className="colorBarsRight"></div>
+                    <div className="outerScreen">
+                        <div className='outerScreen-top'>
+                            <div className='headerCont'>
+                                <div className="colorBarsLeft"></div>
+                                <div className='headerText'>
+                                    <p>DOT MATRIX WITH STEREO SOUND</p>
+                                </div>
+                                <div className="colorBarsRight"></div>
+                            </div>
+                        </div>
+                        <div className='outerScreen-bottom'>
+                            <div className="batteryCont">
+                                <div className="power"></div>
+                                <p className="powerText">BATTERY</p>
+                            </div>
+                            <div className="innerScreen">
+                                {props.children}
+                            </div>
+                        </div>
                     </div>
+                    <div className="controlsCont">
+                    <div className="controlsTop">
+                        <div className="direction">
+                            <i class="fas fa-plus"></i>
+                        </div>
+                        {/* <div className="directionCont">
+                            <h1 className="directionPad">+</h1>
+                        </div> */}
+
+                        <div className="startSelectCont"></div>
+
+                        <div className="buttonCont">
+                            <button className="gameButton-X"></button>
+                            <button className="gameButton-Y"></button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="mainCont">
+                <div className="gameConsole">
+                    
 
                     <div className="outerScreen">
-                        <div className="batteryCont">
-                            <div className="power"></div>
-                            <p className="powerText">BATTERY</p>
+                        <div className="outerscreen-top">
+                            <div className="headerCont">
+                                <div className="colorBarsLeft"></div>
+                                <p className="headerText">
+                                DOT MATRIX WITH STEREO SOUND
+                                </p>
+                                <div className="colorBarsRight"></div>
+                            </div>
                         </div>
-
-                        <div className="innerScreen">
-
-                            {props.children}
-
+                        <div className="outerScreen-bottom">
+                            <div className="batteryCont">
+                                <div className="power"></div>
+                                <p className="powerText">BATTERY</p>
+                            </div>
+                            <div className="innerScreen">
+                                {props.children}
+                            </div>
                         </div>
+                        
                     </div>
 
                     <div className="controlsCont">
@@ -49,7 +93,7 @@ function BaseLayout(props) {
 
                     
                 </div>
-            </div>
+            </div> */}
 
 
         </div>
